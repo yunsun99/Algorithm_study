08cf3b8ed6bd516413db37bf7bcdc1e07d45b4bb
yunsun99 <79686751+yunsun99@users.noreply.github.com>
2025-02-26 10:18:29 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 868 ms, Memory: 232356 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
@@ -1,68 +1,61 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Main {
     public static int[] parent;
-    public static int[] classList;
-    public static int N;
-    public static int M;
-    public static int answer;
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        N = sc.nextInt();
-        M = sc.nextInt();
-        
-        parent = new int[N];
-        classList = new int[N];
-        
-        for(int i=0;i<N;i++) {
+        int N = sc.nextInt();   // 강의 개수
+        int M = sc.nextInt();   // 연결되어 있는 쌍의 개수
+
+        parent = new int[N+1];
+        int[] lectures = new int[N];
+        int answer = 0;
+
+        for (int i = 0; i <= N; i++) {
             parent[i] = i;
         }
-        
-        for(int i=0;i<M ;i++) {
-            int start= sc.nextInt()-1;
-            int end = sc.nextInt()-1;
-            union(start ,end);
+
+        // 연결된 건물 쌍 입력받기
+        for (int k = 0; k < M; k++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+
+            union(i, j);
         }
-        
-        for(int i=0 ;i<N;i++) {
-            classList[i] = sc.nextInt()-1;
+
+        // 마지막 줄 강의코드 입력 받기
+        for (int i = 0; i < N; i++) {
+            lectures[i] = sc.nextInt();
         }
         sc.close();
         
-        for(int i=0;i<N-1;i++) {
-            if(!isSame(classList[i] , classList[i+1])) {
-                answer++;
-            }
+        for (int i = 0; i < N-1; i++) {
+            int prevLecture = lectures[i];
+            int nextLecture = lectures[i+1];
+
+            // if (parent[prevLecture] != parent[nextLecture]) answer++;
+            if (find(prevLecture) != find(nextLecture)) answer++;
         }
+
         System.out.println(answer);
     }
 
+    // 너 부모가 누구양? 어디 그래프출신?
     public static int find(int x) {
-        if(parent[x]==x) {
-            return x;
-        }else {
-            return parent[x] = find(parent[x]);
-        }
+        if (parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
     }
-    
+
+    // 너희 둘은 같은 그래프다!
     public static void union(int x, int y) {
+        // x, y 출신 알아내기
         int rootX = find(x);
         int rootY = find(y);
-        
-        if(rootX != rootY) {
+
+        // 달랐으면 같게 만들어주기!
+        if (rootX != rootY) {
             parent[rootY] = rootX;
         }
     }
-    
-    public static boolean isSame(int x, int y) {
-        if(find(x) == find(y)) {
-            return true;
-        }else {
-            return false;
-        }
-    }
-    
 }
